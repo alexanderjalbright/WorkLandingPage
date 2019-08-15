7313251c3a9ed79a2d5ec88ee42a122a78e0a448
Alex Albright <alexanderjalbright@gmail.com>
2019-08-14 20:07:45 -0400

    Created basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
 
+import Nav from "./Components/Nav";
+import Menu from "./Components/Menu";
+import Alerts from "./Components/Alerts";
+import Trackers from "./Components/Trackers";
+import Notes from "./Components/Notes";
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Menu />
+      <Alerts />
+      <Trackers />
+      <Notes />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Trackers.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Trackers extends Component {
+  render() {
+    return <div className="Trackers">trackers</div>;
+  }
+}
