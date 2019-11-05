7866f39da36dee6e6517f192b02cea0d396b5218
Alex Albright <alexanderjalbright@gmail.com>
2019-11-04 21:46:32 -0500

    TimeMonitor converted

--- src/components/timemonitor/TimeMonitor.js
+++ src/components/timemonitor/TimeMonitor.js
@@ -1,67 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import TimeBar from "./TimeBar";
 import HolidaySlideShow from "./HolidaySlideShow";
 
-export default class TimeMonitor extends Component {
-  constructor() {
-    super();
-    this.state = {
-      dayRemTime: "",
-      dayRemPercent: "",
-      dayElaptime: "",
-      dayElapPercent: "",
-      weekRemTime: "",
-      weekRemPercent: "",
-      weekElapTime: "",
-      weekElapPercent: ""
-    };
-  }
-  render() {
-    return (
-      <div
-        className="time-monitor"
-        style={{
-          backgroundColor: this.props.timeMonitorColor,
-          gridArea: "time",
-          display: "grid",
-          gridTemplateAreas: "'title' 'Day' 'Week' 'holiday'",
-          gridTemplateRows: "1fr 2fr 2fr 1fr",
-          gridTemplateColumns: "100%",
-          padding: "10px 10%"
-        }}
-      >
-        <div className="card-title">Time Monitor</div>
+export default function TimeMonitor(props) {
+  const { startTime, endTime, timeMonitorColor } = props;
+  const [dayTracker, setDayTracker] = useState({
+    dayRemTime: "",
+    dayRemPercent: "",
+    dayElaptime: "",
+    dayElapPercent: ""
+  });
+  const [weekTracker, setWeekTracker] = useState({
+    weekRemTime: "",
+    weekRemPercent: "",
+    weekElapTime: "",
+    weekElapPercent: ""
+  });
 
-        <TimeBar
-          remainingPercent={this.state.dayRemPercent}
-          remainingTime={this.state.dayRemTime}
-          elapsedPercent={this.state.dayElapPercent}
-          elapsedTime={this.state.dayElapTime}
-          gridArea="Day"
-        />
-        <TimeBar
-          remainingPercent={this.state.weekRemPercent}
-          remainingTime={this.state.weekRemTime}
-          elapsedPercent={this.state.weekElapPercent}
-          elapsedTime={this.state.weekElapTime}
-          gridArea="Week"
-        />
-        <HolidaySlideShow holidays={this.props.holidays} removeHoliday={this.props.removeHoliday}/>
-      </div>
-    );
-  }
+  const [isInit, setIsInit] = useState(false);
 
-  componentDidMount = () => {
-    this.runTimeMonitor();
-    setInterval(this.runTimeMonitor, 1000);
-  };
+  useEffect(isInit => {
+    if (!isInit) {
+      runTimeMonitor();
+      setInterval(runTimeMonitor, 1000);
+      setIsInit(true);
+    }
+  }, []);
 
-  convertSecs = (hr, min, sec) => {
+  const convertSecs = (hr, min, sec) => {
     const seconds = (hr * 60 + min * 1) * 60 + sec * 1;
     return seconds;
   };
 
-  secondsTohhmmss = totalSeconds => {
+  const secondsTohhmmss = totalSeconds => {
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds - hours * 3600) / 60);
     let seconds = totalSeconds - hours * 3600 - minutes * 60;
@@ -73,37 +44,37 @@ export default class TimeMonitor extends Component {
     return result;
   };
 
-  splitTime = time => {
+  const splitTime = time => {
     const times = time.split(":");
-    const result = this.convertSecs(times[0], times[1], times[2]);
+    const result = convertSecs(times[0], times[1], times[2]);
     return result;
   };
 
-  runTimeMonitor = () => {
+  const runTimeMonitor = () => {
     const now = new Date();
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
 
-    this.dayTimeMonitor(hours, minutes, seconds);
-    this.weekTimeMonitor(hours, minutes, seconds, now);
+    dayTimeMonitor(hours, minutes, seconds);
+    weekTimeMonitor(hours, minutes, seconds, now);
   };
 
-  dayTimeMonitor = (hours, minutes, seconds) => {
-    const nowSeconds = this.convertSecs(hours, minutes, seconds);
-    const startTimeSeconds = this.splitTime(this.props.startTime);
+  const dayTimeMonitor = (hours, minutes, seconds) => {
+    const nowSeconds = convertSecs(hours, minutes, seconds);
+    const startTimeSeconds = splitTime(startTime);
 
-    const endTimeSeconds = this.splitTime(this.props.endTime);
+    const endTimeSeconds = splitTime(endTime);
 
     let elapsedPercent = 0;
     let elapsedTime = "0:00:00";
     let remainingPercent = 100;
-    let remainingTime = this.secondsTohhmmss(endTimeSeconds - startTimeSeconds);
+    let remainingTime = secondsTohhmmss(endTimeSeconds - startTimeSeconds);
     if (nowSeconds > endTimeSeconds) {
       elapsedPercent = 100;
       remainingPercent = 0;
       remainingTime = "0:00:00";
-      elapsedTime = this.secondsTohhmmss(endTimeSeconds - startTimeSeconds);
+      elapsedTime = secondsTohhmmss(endTimeSeconds - startTimeSeconds);
     } else if (nowSeconds > startTimeSeconds) {
       elapsedPercent = (
         ((nowSeconds - startTimeSeconds) /
@@ -111,11 +82,11 @@ export default class TimeMonitor extends Component {
         100
       ).toFixed(1);
       remainingPercent = (100 - elapsedPercent).toFixed(1);
-      remainingTime = this.secondsTohhmmss(endTimeSeconds - nowSeconds);
-      elapsedTime = this.secondsTohhmmss(nowSeconds - startTimeSeconds);
+      remainingTime = secondsTohhmmss(endTimeSeconds - nowSeconds);
+      elapsedTime = secondsTohhmmss(nowSeconds - startTimeSeconds);
     }
 
-    this.setState({
+    setDayTracker({
       dayRemTime: remainingTime,
       dayRemPercent: remainingPercent,
       dayElapTime: elapsedTime,
@@ -123,16 +94,16 @@ export default class TimeMonitor extends Component {
     });
   };
 
-  weekTimeMonitor = (hours, minutes, seconds, now) => {
+  const weekTimeMonitor = (hours, minutes, seconds, now) => {
     const nowSeconds = new Date().setHours(hours, minutes, seconds);
-    const startHMSarr = this.props.startTime.split(":");
-    const endHMSarr = this.props.endTime.split(":");
-    const startTimeSeconds = this.getWeekday(now, 1).setHours(
+    const startHMSarr = startTime.split(":");
+    const endHMSarr = endTime.split(":");
+    const startTimeSeconds = getWeekday(now, 1).setHours(
       startHMSarr[0],
       startHMSarr[1],
       startHMSarr[2]
     );
-    const endTimeSeconds = this.getWeekday(now, 5).setHours(
+    const endTimeSeconds = getWeekday(now, 5).setHours(
       endHMSarr[0],
       endHMSarr[1],
       endHMSarr[2]
@@ -157,7 +128,7 @@ export default class TimeMonitor extends Component {
       remainingTime = `${((remainingPercent / 100) * 5).toFixed(1)}`;
       elapsedTime = `${((elapsedPercent / 100) * 5).toFixed(1)}`;
     }
-    this.setState({
+    setWeekTracker({
       weekRemTime: remainingTime,
       weekRemPercent: remainingPercent,
       weekElapTime: elapsedTime,
@@ -165,10 +136,46 @@ export default class TimeMonitor extends Component {
     });
   };
 
-  getWeekday = (date, weekdayZeroth) => {
+  const getWeekday = (date, weekdayZeroth) => {
     let day = date.getDay();
     let diff =
       date.getDate() - day + (day === 0 ? weekdayZeroth - 7 : weekdayZeroth); // adjust when day is sunday
     return new Date(date.setDate(diff));
   };
+
+  return (
+    <div
+      className="time-monitor"
+      style={{
+        backgroundColor: timeMonitorColor,
+        gridArea: "time",
+        display: "grid",
+        gridTemplateAreas: "'title' 'Day' 'Week' 'holiday'",
+        gridTemplateRows: "1fr 2fr 2fr 1fr",
+        gridTemplateColumns: "100%",
+        padding: "10px 10%"
+      }}
+    >
+      <div className="card-title">Time Monitor</div>
+
+      <TimeBar
+        remainingPercent={dayTracker.dayRemPercent}
+        remainingTime={dayTracker.dayRemTime}
+        elapsedPercent={dayTracker.dayElapPercent}
+        elapsedTime={dayTracker.dayElapTime}
+        gridArea="Day"
+      />
+      <TimeBar
+        remainingPercent={weekTracker.weekRemPercent}
+        remainingTime={weekTracker.weekRemTime}
+        elapsedPercent={weekTracker.weekElapPercent}
+        elapsedTime={weekTracker.weekElapTime}
+        gridArea="Week"
+      />
+      <HolidaySlideShow
+        holidays={props.holidays}
+        removeHoliday={props.removeHoliday}
+      />
+    </div>
+  );
 }
